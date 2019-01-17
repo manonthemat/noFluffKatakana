81bc3b23779ab49d782580fab312500cf5d3fe99
Matthias Sieber <matthiasksieber@gmail.com>
2019-01-16 23:53:43 -0800

    Squashed commit of the following:
    commit 8665d6f7d2905f0ace398a6523f85abef3fded90
    Author: Matthias Sieber <matthiasksieber@gmail.com>
    Date:   Wed Jan 16 23:52:53 2019 -0800
    
        1-min gimp'd assets
    
    commit dd1d2921bb29262c8a6d0e6ae5c85c7598fe7fc7
    Author: Matthias Sieber <matthiasksieber@gmail.com>
    Date:   Wed Jan 16 23:29:18 2019 -0800
    
        styling changes
    
    commit 9682af3bf70a443717c5f3fc2745b0489c1332a4
    Author: Matthias Sieber <matthiasksieber@gmail.com>
    Date:   Wed Jan 16 22:19:40 2019 -0800
    
        minor changes
    
        including a bugfix that allowed the potential for a choice to show up more than once
    
    commit 1d1ebeec3deea116cdf2dab30d263101159ebe29
    Author: Matthias Sieber <matthiasksieber@gmail.com>
    Date:   Wed Jan 16 21:08:46 2019 -0800
    
        initial App commit
    
        working product, but could use some cleanup and better design choices

--- App.js
+++ App.js
@@ -1,21 +1,162 @@
+'use strict';
+
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, TouchableOpacity } from 'react-native';
+import shuffle from 'knuth';
+import KatakanaMap from './katakanamap';
 
 export default class App extends React.Component {
+  defaultState = () => {
+    return {
+      katakanaToSolve: shuffle(Object.keys(KatakanaMap)),
+      correct: 0,
+      total: 0
+    };
+  };
+  state = this.defaultState();
+  reset = () => {
+    this.setState(this.defaultState());
+  };
+  rightChoiceSelected = () => {
+    this.setState({
+      correct: this.state.correct + 1,
+      katakanaToSolve: this.state.katakanaToSolve.splice(1),
+      total: this.state.total + 1
+    });
+  };
+  wrongChoiceSelected = () => {
+    this.setState({
+      katakanaToSolve: this.state.katakanaToSolve.splice(1),
+      total: this.state.total + 1
+    });
+  };
+  showKatakanaToSolve = () => (<View><Text style={styles.katakana}>{this.state.katakanaToSolve[0]}</Text></View>);
+
+  // TODO: refactor to bring down cyclomatix complexity to 2 (currently it's at 3)
+  showChoices = () => {
+    const KatakanaToSolve = this.state.katakanaToSolve[0];
+    const CorrectAnswer = KatakanaMap[KatakanaToSolve];
+
+    // build some noise (wrong answers)
+    const wrongChoices = [];
+    while (wrongChoices.length < 3) {
+      const randomKatakana = pickRandomProperty(KatakanaMap);
+      if (randomKatakana !== KatakanaToSolve && !wrongChoices.includes(KatakanaMap[randomKatakana])) {
+        wrongChoices.push(KatakanaMap[randomKatakana]);
+      }
+    }
+
+    // build the elements
+    const allChoices = wrongChoices.concat(CorrectAnswer);
+    shuffle(allChoices);
+
+    const renderChoices = () => {
+      return allChoices.map(choice => {
+        return (
+          <TouchableOpacity
+            style={styles.button}
+            onPress={choice !== CorrectAnswer ? this.wrongChoiceSelected : this.rightChoiceSelected}
+            key={choice}
+          >
+            <Text>{choice}</Text>
+          </TouchableOpacity>
+        );
+      });
+    };
+
+    return (
+      <View style={styles.choices}>
+        {renderChoices()}
+      </View>
+    );
+  };
+
   render() {
+    const { correct, total } = this.state;
+    const remaining = this.state.katakanaToSolve.length;
+    const show = () => {
+      if (remaining) {
+        return (
+          <View style={styles.main}>
+            {this.showKatakanaToSolve()}
+            {this.showChoices()}
+          </View>
+        );
+      } else {
+        const correctPerc = Math.round(correct / total * 100);
+        return (
+          <View style={styles.main}>
+            <Text>{correctPerc}% correct!</Text>
+            <TouchableOpacity onPress={this.reset} style={styles.button}>
+              <Text>Try Again</Text>
+            </TouchableOpacity>
+          </View>
+        );
+      }
+    };
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <View style={styles.info}>
+          <View><Text style={styles.infoText}>Correct: {correct} / {total}</Text></View>
+          <View><Text style={styles.infoText}>Remaining: {remaining}</Text></View>
+        </View>
+        {show()}
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
+const styles = {
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#F5FCFF',
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  katakana: {
+    fontSize: 100,
+    color: 'black'
+  },
+  choices: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+    flexWrap: 'wrap'
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: 'white',
+    borderColor: '#CCC',
+    borderRadius: 12,
+    borderWidth: 1,
+    color: 'white',
+    justifyContent: 'space-around',
+    margin: 6,
+    overflow: 'hidden',
+    padding: 10,
+    width: 180
+  },
+  info: {
+    alignSelf: 'stretch',
+    alignItems: 'flex-end',
+    justifyContent: 'space-around',
+    flexDirection: 'row',
+    flex: 2
+  },
+  infoText: {
+    fontSize: 8
+  },
+  main: {
+    flex: 23,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+};
+
+const pickRandomProperty = obj => {
+  let result;
+  let count = 0;
+  for (const prop in obj)
+    if (Math.random() < 1 / ++count) result = prop;
+  return result;
+};
